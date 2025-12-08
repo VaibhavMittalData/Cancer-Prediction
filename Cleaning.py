d8c3dc69b7600117f958b360dc69b52166d998dc
Vaibhav Mittal <vaibhavmittal55552@gmail.com>
2025-12-08 15:58:55 +0530

    Add files via upload

---
+++ Cleaning.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+
+df = pd.read_csv(r"Project.py/lung_cancer_dataset.csv")
+
+print(df.info())
+ 
+df.drop(columns=['patient_id'], inplace=True)
+df['alcohol_consumption'].fillna('Not drinking', inplace=True)
+print(df.info())
+print(df.describe())
+print(df.head())
+print(df['alcohol_consumption'].unique())
+print(df.info())
+print(df['lung_cancer'].value_counts())
+
+sns.boxplot(x=df['pack_years'])
+plt.title("Outliers in pack_years")
+plt.show()
+
+
+
+
+
+# corr_target = df.corr()['lung_cancer'].sort_values(ascending=False)
+# print("\nCorrelation with Lung Cancer:\n")
+# print(corr_target)
+# print(df.head())
+
+df.to_csv("processed_lung_cancer.csv", index=False)
